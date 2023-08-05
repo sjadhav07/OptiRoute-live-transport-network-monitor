975a00ddb75c7eb413bc0d3759a83268aeefd2b7
Sudarshan Jadhav <sudarshanjadhav1821@gmail.com>
2023-08-06 01:05:30 +0530

    Send first STOMP frame and add JSON library to parse file

--- conanfile.py
+++ conanfile.py
@@ -1,13 +1,16 @@
 from conans import ConanFile, CMake
 
 class ConanPackage(ConanFile):
-    name = "OptiRoute-live-transport-network-monitor"
+    name = 'network-monitor'
     version = "0.1.0"
 
-    generators = "cmake_find_package"
+    generators = 'cmake_find_package'
 
     requires = [
         ('boost/1.74.0'),
+        ("libcurl/7.73.0"),
+        ('openssl/1.1.1h'),
+         ('zlib/1.2.13'),
     ]
 
     default_options = (
